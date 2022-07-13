9318fa6c8e1cb39444957707ccc927412101b928
Daniel JS <daniel@mydeveloperjourney.com>
2022-07-12 22:02:42 -0500

    wip: adds basic config

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 // require dependencies
+const express = require('express');
 
 // initialize the express app
+const app = express();
 
 // configure application settings
 
 // mount middleware
 
 // mount routes
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>');
+});
 
 // tell the app to listen for requests from the client
+
+// ? why do we do this ?
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Express is listening for request on port:${port}`);
+});
