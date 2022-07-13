3ce26bc17660c9cbbfc51e2b69fc0155e93c4507
Daniel JS <daniel@mydeveloperjourney.com>
2022-07-12 21:49:30 -0500

    wip: makes initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// require dependencies
+
+// initialize the express app
+
+// configure application settings
+
+// mount middleware
+
+// mount routes
+
+// tell the app to listen for requests from the client
